5f17bf70afb77483a0a9fec4445bafcddcd00942
santiago <sosasantiagottc@gmail.com>
2024-08-09 21:08:44 -0300

    Agrego metodos get, patch, delete. Se corrigio la db

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('sistema', 'root', '',{
+const db = new Sequelize('equipo', 'root', '',{
     host:'localhost',
     dialect:'mysql'
 })
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { createArticle, getAllArticle } from '../controllers/articuloController.js'
+import { createArticle, deleteArticle, getAllArticle, getArticle, updateArticle } from '../controllers/articuloController.js'
 
 const router = express.Router()
 
 router.get('/', getAllArticle)
+router.get('/:id', getArticle)
 router.post('/', createArticle)
+router.patch('/:id', updateArticle)
+router.delete('/:id', deleteArticle)
 
 export default router
